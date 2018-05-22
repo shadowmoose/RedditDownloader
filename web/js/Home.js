dad2406cf21c3acedd74709bbaddf1b865aa07d4
shadowmoose <theshadowmoose@gmail.com>
2018-05-22 16:56:36 -0700

    Base HTML for WebUI.

---
+++ web/js/Home.js
@@ -0,0 +1,15 @@
+class Home extends React.Component {
+	render() {
+		return (
+			<div>
+				<h2>Home</h2>
+				<p>asnfkasndkfjnksnfajsfnd
+					kjsnf est klmfslm sf:</p>
+				<ol>
+					<li>Nulla pulvinar diam</li>
+					<li>Facilisis bibendum</li>
+				</ol>
+			</div>
+		);
+	}
+}
