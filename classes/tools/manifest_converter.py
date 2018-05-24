78ac790a7e6d83a26a9511179f8991664af5f801
ShadowMoose <theshadowmoose@gmail.com>
2018-05-23 20:27:29 -0700

    More settings refactoring.

--- classes/tools/manifest_converter.py
+++ classes/tools/manifest_converter.py
@@ -1,6 +1,5 @@
 import gzip
 import json
-from classes.util.settings import Settings
 from classes.util import stringutil
 from classes.util import manifest
 from classes.reddit import reddit as rd
@@ -65,21 +64,3 @@ def convert(save_base, data):
 		_converted += 1
 	print('Comments that cannot be converted: %s' % broken)
 
-
-if __name__ == '__main__':
-	#  All of this is just to stage everything as if it were running live.
-	_verbose = True
-	import os
-	sf = input('Enter the exact path to your settings file: ')
-	_set = Settings(sf, can_save=False)
-	os.chdir(os.path.dirname(sf))
-	os.chdir(_set.save_base())
-	auth_info = _set.get('auth')
-	manifest.create('manifest.sqldb')
-	rd.init(client_id=auth_info['client_id'], client_secret=auth_info['client_secret'],
-				password=auth_info['password'], user_agent=auth_info['user_agent'], username=auth_info['username'])
-	rd.login()
-	_data = load(input('Enter the exact path to the old Manifest file: '))
-	print(_data)
-	convert(_set.save_base(), _data)
-	# TODO: Replicate this in a Test, to make sure it's always working.
