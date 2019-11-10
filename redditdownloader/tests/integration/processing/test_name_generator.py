3149b02b312ec45be472a87d4873c5c0c935e407
ShadowMoose <theshadowmoose@gmail.com>
2019-11-10 00:06:51 -0700

    Added support for created_date and created_time filepath tags.

--- redditdownloader/tests/integration/processing/test_name_generator.py
+++ redditdownloader/tests/integration/processing/test_name_generator.py
@@ -43,10 +43,10 @@ class NameGeneratorTest(EnvironmentTest):
 
 	def test_pattern_loader(self):
 		""" All pattern tags should work """
-		settings.put('output.file_name_pattern', '[type]-[reddit_id]-[title]-[author]-[subreddit]-[source_alias]-[created_utc]')
+		settings.put('output.file_name_pattern', '[type]-[reddit_id]-[title]-[author]-[subreddit]-[source_alias]-[created_utc]-[created_date]-[created_time]')
 		tp = self.sess.query(sql.Post).filter(sql.Post.title == 'test').first()
 		file = ng.choose_file_name(tp.urls[0], tp, sql.session(), album_size=1)
-		self.assertEqual('Submission-t3_b1rycu-test-testuser-aww-newsource-1552739416', file, msg='Failed to convert basic Test post!')
+		self.assertEqual('Submission-t3_b1rycu-test-testuser-aww-newsource-1552739416-2019-03-16-05.30.16', file, msg='Failed to convert basic Test post!')
 
 	def test_pattern_fail_load(self):
 		""" Invalid patterns should fail """
